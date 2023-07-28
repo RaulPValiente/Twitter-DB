cb54929bd11b9f8a2df7ccc2c4613fb77668076f
Raul <raulperezvaliente95@gmail.com>
2023-07-28 13:32:19 +0200

    database creation

---
+++ twitter.sql
@@ -0,0 +1,77 @@
+show databases;
+
+DROP DATABASE IF EXISTS twitter_db;
+
+CREATE DATABASE twitter_db;
+
+USE twitter_db;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+	user_id INT NOT NULL AUTO_INCREMENT,
+	user_handle VARCHAR(20) NOT NULL UNIQUE,
+    email_address VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL UNIQUE,
+    follower_count INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT (now()),
+    PRIMARY KEY (user_id)
+);
+
+DROP TABLE IF EXISTS followers;
+
+CREATE TABLE followers (
+	follower_id INT NOT NULL,
+    following_id INT NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users (user_id),
+    FOREIGN KEY (following_id) REFERENCES users (user_id),
+    PRIMARY KEY (follower_id, following_id)
+);
+
+ALTER TABLE followers
+ADD CONSTRAINT check_follower_id
+CHECK (follower_id <> following_id);
+
+DROP TABLE IF EXISTS tweets;
+
+CREATE TABLE tweets (
+	tweet_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    tweet_text VARCHAR(280) NOT NULL,
+    num_likes INT DEFAULT 0,
+    num_rt INT DEFAULT 0,
+    num_comments INT DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT (now()),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    PRIMARY KEY (tweet_id)
+);
+
+DROP TABLE IF EXISTS tweet_likes;
+
+CREATE TABLE tweet_likes (
+	user_id INT NOT NULL,
+    tweet_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (tweet_id) REFERENCES tweets (tweet_id),
+    PRIMARY KEY (user_id, tweet_id)
+);
+
+DROP TRIGGER IF EXISTS increase_follower_count;
+
+DELIMITER $$
+
+CREATE TRIGGER increase_follower_count
+	AFTER INSERT ON followers
+    FOR EACH ROW
+    BEGIN
+		UPDATE users SET follower_count = follower_count + 1
+        WHERE user_id = NEW.following_id;
+    END $$
+    
+DELIMITER ;
+
+
+
+
